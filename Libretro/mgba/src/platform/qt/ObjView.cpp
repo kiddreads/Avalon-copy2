mgba-emu/mgba

Qt: Fix sprite export pausing game indefinitely (fixes #841)

src/platform/qt/ObjView.cpp
@@ -248,7 +248,6 @@ void ObjView::updateTilesGB(bool force) {
 
 #ifdef USE_PNG
 void ObjView::exportObj() {
-	CoreController::Interrupter interrupter(m_controller);
 	QString filename = GBAApp::app()->getSaveFileName(this, tr("Export sprite"),
 	                                                  tr("Portable Network Graphics (*.png)"));
 	VFile* vf = VFileDevice::open(filename, O_WRONLY | O_CREAT | O_TRUNC);
@@ -257,6 +256,7 @@ void ObjView::exportObj() {
 		return;
 	}
 
+	CoreController::Interrupter interrupter(m_controller);
 	mTileCacheSetPalette(m_tileCache, m_objInfo.paletteSet);
 	png_structp png = PNGWriteOpen(vf);
 	png_infop info = PNGWriteHeader8(png, m_objInfo.width * 8, m_objInfo.height * 8);

kiddreads/Avalon-copy2

Libretro/mgba/src/platform/qt/ObjView.cpp
@@ -247,7 +247,6 @@ void ObjView::updateTilesGB(bool force) {
 
 #ifdef USE_PNG
 void ObjView::exportObj() {
-	GameController::Interrupter interrupter(m_controller);
 	QString filename = GBAApp::app()->getSaveFileName(this, tr("Export sprite"),
 	                                                  tr("Portable Network Graphics (*.png)"));
 	VFile* vf = VFileDevice::open(filename, O_WRONLY | O_CREAT | O_TRUNC);
@@ -256,6 +255,7 @@ void ObjView::exportObj() {
 		return;
 	}
 
+	GameController::Interrupter interrupter(m_controller);
 	mTileCacheSetPalette(m_tileCache.get(), m_objInfo.paletteSet);
 	png_structp png = PNGWriteOpen(vf);
 	png_infop info = PNGWriteHeader8(png, m_objInfo.width * 8, m_objInfo.height * 8);