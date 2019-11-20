kiddreads/Avalon-copy2

commit 6c66595c55b0e47b175d3a69a9e40ba641fe377e

GB: Add yanking

Libretro/mgba/src/platform/qt/CoreController.cpp
@@ -628,13 +628,16 @@ void CoreController::replaceGame(const QString& path) {
 }
 
 void CoreController::yankPak() {
-#ifdef M_CORE_GBA
-	if (platform() != PLATFORM_GBA) {
-		return;
-	}
 	Interrupter interrupter(this);
-	GBAYankROM(static_cast<GBA*>(m_threadContext.core->board));
-#endif
+
+	switch (platform()) {
+	case PLATFORM_GBA:
+		GBAYankROM(static_cast<GBA*>(m_threadContext.core->board));
+		break;
+	case PLATFORM_GB:
+		GBYankROM(static_cast<GB*>(m_threadContext.core->board));
+		break;
+	}
 }
 
 void CoreController::addKey(int key) {

Libretro/mgba/include/mgba/internal/gb/gb.h
@@ -94,6 +94,7 @@ struct GB {
 	bool isPristine;
 	size_t pristineRomSize;
 	size_t yankedRomSize;
+	enum GBMemoryBankControllerType yankedMbc;
 	uint32_t romCrc32;
 	struct VFile* romVf;
 	struct VFile* biosVf;
@@ -163,6 +164,7 @@ bool GBLoadROM(struct GB* gb, struct VFile* vf);
 bool GBLoadSave(struct GB* gb, struct VFile* vf);
 void GBUnloadROM(struct GB* gb);
 void GBSynthesizeROM(struct VFile* vf);
+void GBYankROM(struct GB* gb);
 
 void GBLoadBIOS(struct GB* gb, struct VFile* vf);
 

Libretro/mgba/src/gb/gb.c
@@ -127,6 +127,19 @@ bool GBLoadROM(struct GB* gb, struct VFile* vf) {
 	return true;
 }
 
+void GBYankROM(struct GB* gb) {
+	gb->yankedRomSize = gb->memory.romSize;
+	gb->yankedMbc = gb->memory.mbcType;
+	gb->memory.romSize = 0;
+	gb->memory.mbcType = GB_MBC_NONE;
+	gb->memory.sramAccess = false;
+
+	if (gb->cpu) {
+		struct LR35902Core* cpu = gb->cpu;
+		cpu->memory.setActiveRegion(cpu, cpu->pc);
+	}
+}
+
 static void GBSramDeinit(struct GB* gb) {
 	if (gb->sramVf) {
 		gb->sramVf->unmap(gb->sramVf, gb->memory.sram, gb->sramSize);
@@ -430,6 +443,7 @@ void GBReset(struct LR35902Core* cpu) {
 
 	if (gb->yankedRomSize) {
 		gb->memory.romSize = gb->yankedRomSize;
+		gb->memory.mbcType = gb->yankedMbc;
 		gb->yankedRomSize = 0;
 	}
 

Libretro/mgba/src/gb/io.c
@@ -458,6 +458,9 @@ void GBIOWrite(struct GB* gb, unsigned address, uint8_t value) {
 		value = gb->video.stat;
 		break;
 	case 0x50:
+		if (gb->memory.io[0x50] != 0xFF) {
+			break;
+		}
 		GBUnmapBIOS(gb);
 		if (gb->model >= GB_MODEL_CGB && gb->memory.io[REG_UNK4C] < 0x80) {
 			gb->model = GB_MODEL_DMG;

Libretro/mgba/src/gb/memory.c
@@ -16,6 +16,8 @@
 
 mLOG_DEFINE_CATEGORY(GB_MEM, "GB Memory", "gb.memory");
 
+static const uint8_t _yankBuffer[] = { 0xFF };
+
 enum GBBus {
 	GB_BUS_CPU,
 	GB_BUS_MAIN,
@@ -69,6 +71,14 @@ static void GBSetActiveRegion(struct LR35902Core* cpu, uint16_t address) {
 		cpu->memory.activeRegion = memory->romBase;
 		cpu->memory.activeRegionEnd = GB_BASE_CART_BANK1;
 		cpu->memory.activeMask = GB_SIZE_CART_BANK0 - 1;
+		if (gb->memory.romSize < GB_SIZE_CART_BANK0) {
+			if (address >= gb->memory.romSize) {
+				cpu->memory.activeRegion = _yankBuffer;
+				cpu->memory.activeMask = 0;
+			} else {
+				cpu->memory.activeRegionEnd = gb->memory.romSize;
+			}
+		}
 		break;
 	case GB_REGION_CART_BANK1:
 	case GB_REGION_CART_BANK1 + 1:
@@ -87,6 +97,14 @@ static void GBSetActiveRegion(struct LR35902Core* cpu, uint16_t address) {
 			} else {
 				cpu->memory.activeRegion = memory->romBank;
 				cpu->memory.activeRegionEnd = GB_BASE_CART_BANK1 + 0x2000;
+			}
+		}
+		if (gb->memory.romSize < GB_SIZE_CART_BANK0 * 2) {
+			if (address >= gb->memory.romSize) {
+				cpu->memory.activeRegion = _yankBuffer;
+				cpu->memory.activeMask = 0;
+			} else {
+				cpu->memory.activeRegionEnd = gb->memory.romSize;
 			}
 		}
 		break;
@@ -243,6 +261,9 @@ uint8_t GBLoad8(struct LR35902Core* cpu, uint16_t address) {
 	case GB_REGION_CART_BANK0 + 1:
 	case GB_REGION_CART_BANK0 + 2:
 	case GB_REGION_CART_BANK0 + 3:
+		if (address >= memory->romSize) {
+			return 0xFF;
+		}
 		return memory->romBase[address & (GB_SIZE_CART_BANK0 - 1)];
 	case GB_REGION_CART_BANK1 + 2:
 	case GB_REGION_CART_BANK1 + 3:
@@ -252,6 +273,9 @@ uint8_t GBLoad8(struct LR35902Core* cpu, uint16_t address) {
 		// Fall through
 	case GB_REGION_CART_BANK1:
 	case GB_REGION_CART_BANK1 + 1:
+		if (address >= memory->romSize) {
+			return 0xFF;
+		}
 		return memory->romBank[address & (GB_SIZE_CART_BANK0 - 1)];
 	case GB_REGION_VRAM:
 	case GB_REGION_VRAM + 1:

Libretro/mgba/src/platform/qt/Window.cpp
@@ -1212,12 +1212,10 @@ void Window::setupMenu(QMenuBar* menubar) {
 		m_controller->stop();
 	}, "emu");
 
-#ifdef M_CORE_GBA
 	Action* yank = addGameAction(tr("Yank game pak"), "yank", [this]() {
 		m_controller->yankPak();
 	}, "emu");
-	m_platformActions.insert(PLATFORM_GBA, yank);
-#endif
+
 	m_actions.addSeparator("emu");
 
 	Action* pause = m_actions.addBooleanAction(tr("&Pause"), "pause", [this](bool paused) {

commit 244a19ba41525e3c324cdae23071a25de1987e44

Qt: Fix compiling with M_CORE_GB disabled
GB specific code was added without guards, causing configurations with
it disabled to fail compiling:

    cmake -B build -DM_CORE_GB:BOOL=OFF

Resolves: #1578
Fixes: 6c66595c55b0e ("GB: Add yanking")
Fixes: e209591b1c814 ("Core: Add support for caching bitmapped modes")
Fixes: 40a8bf72026d9 ("Qt: Add additional info to map view")
Fixes: 249775155fe0c ("Qt: Set default Game Boy colors")

Libretro/mgba/src/platform/qt/CoreController.cpp
@@ -632,12 +632,16 @@ void CoreController::yankPak() {
 	Interrupter interrupter(this);
 
 	switch (platform()) {
+#ifdef M_CORE_GBA
 	case PLATFORM_GBA:
 		GBAYankROM(static_cast<GBA*>(m_threadContext.core->board));
 		break;
+#endif
+#ifdef M_CORE_GB
 	case PLATFORM_GB:
 		GBYankROM(static_cast<GB*>(m_threadContext.core->board));
 		break;
+#endif
 	}
 }
 

Libretro/mgba/src/platform/qt/MapView.cpp
@@ -167,6 +167,7 @@ void MapView::updateTilesGBA(bool force) {
 		int frame = 0;
 		QString offset(tr("N/A"));
 		QString transform(tr("N/A"));
+#ifdef M_CORE_GBA
 		if (m_controller->platform() == PLATFORM_GBA) {
 			uint16_t* io = static_cast<GBA*>(m_controller->thread()->core->board)->memory.io;
 			int mode = GBARegisterDISPCNTGetMode(io[REG_DISPCNT >> 1]);
@@ -199,12 +200,15 @@ void MapView::updateTilesGBA(bool force) {
 
 			}
 		}
+#endif
+#ifdef M_CORE_GB
 		if (m_controller->platform() == PLATFORM_GB) {
 			uint8_t* io = static_cast<GB*>(m_controller->thread()->core->board)->memory.io;
 			int x = io[m_map == 0 ? 0x42 : 0x4A];
 			int y = io[m_map == 0 ? 0x43 : 0x4B];
 			offset = QString("%1, %2").arg(x).arg(y);
 		}
+#endif
 		if (bitmap >= 0) {
 			mBitmapCache* bitmapCache = mBitmapCacheSetGetPointer(&m_cacheSet->bitmaps, bitmap);
 			int width = mBitmapCacheSystemInfoGetWidth(bitmapCache->sysConfig);

Libretro/mgba/src/platform/qt/OverrideView.cpp
@@ -69,6 +69,7 @@ OverrideView::OverrideView(ConfigController* config, QWidget* parent)
 		m_ui.hwRumble->setEnabled(!enabled);
 	});
 
+#ifdef M_CORE_GB
 	m_colorPickers[0] = ColorPicker(m_ui.color0, QColor(0xF8, 0xF8, 0xF8));
 	m_colorPickers[1] = ColorPicker(m_ui.color1, QColor(0xA8, 0xA8, 0xA8));
 	m_colorPickers[2] = ColorPicker(m_ui.color2, QColor(0x50, 0x50, 0x50));
@@ -86,6 +87,7 @@ OverrideView::OverrideView(ConfigController* config, QWidget* parent)
 			m_gbColors[colorId] = color.rgb() | 0xFF000000;
 		});
 	}
+#endif
 
 #ifndef M_CORE_GBA
 	m_ui.tabWidget->removeTab(m_ui.tabWidget->indexOf(m_ui.tabGBA));